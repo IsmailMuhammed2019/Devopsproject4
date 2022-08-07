cc391bc19555e474ac19683a3fcfd8f0d1daeb01
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-08-07 06:51:24 +0100

    update code

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath="ecoden/sklearn-ml-api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-microk8s kubectl run sklearn-pod --image=$dockerpath:latest --port=80
+microk8s kubectl run deveopsproject4 --image=$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ microk8s kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-microk8s kubectl port-forward sklearn-pod 8000:80
+microk8s kubectl port-forward deveopsproject4 8000:80

cb6d3797af72ec0fdbd1d4218629ff0823788ae1
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-08-07 06:43:46 +0100

    completed task to upload docker image to docker hub

--- run_docker.sh
+++ run_docker.sh
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 --name sklearn ecoden/sklearn-ml-api
+docker run -p 8000:80 --name devopsproject4 ecoden/sklearn-ml-api
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ecoden/sklearn-ml-api"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $dockerpath:v1.0 $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
